75003a3db1fff6c055668d447ea389cd4522bf14
Serhii Yakovliuk <serhii.y.m@gmail.com>
2022-08-08 13:39:17 +0400

    Added a logic into fibonacci feature

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,3 +1,7 @@
+let inputNumber = document.getElementById("number");
+let outputResult = document.getElementById("result");
+let lastNumber = document.getElementById("lastNumber");
+
 function fibonacci(n) {
     if(n === 0) {
         return 0;
@@ -11,11 +15,28 @@ function fibonacci(n) {
 }
 
 function fibonacciRow(n) {
-    let arr = [];
+
+    let result =[];
     for(let i = 0; i < n; i++) {
-        arr[i] = fibonacci(i);
+        if(i === 0) {
+            result.push(0);
+        }
+        else if(i === 1) {
+            result.push(1);
+        }
+        else if (i > 1){
+            let num = result[i-1]+result[i-2];
+            result.push(num);
+        }
     }
-    return arr;
+    return result;
 }
 
-console.log(fibonacciRow(13));
+function outFibonacci() {
+
+    let num = inputNumber.value;
+    let resultRow = fibonacciRow(num);
+    outputResult.innerHTML = resultRow;
+    lastNumber.innerHTML = "Last number is <b>" + resultRow.splice(-1) + "</b>";
+
+}
